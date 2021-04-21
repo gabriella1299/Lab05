7187e9dd4cce589dae50b30b836054ba646d4e6d
nagab <nagab@LAPTOP-MT1TCD0G>
2021-04-21 21:47:44 +0200

    Connection pooling

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
@@ -1,14 +1,34 @@
 package it.polito.tdp.anagrammi.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnect {
+import com.zaxxer.hikari.HikariDataSource;
 
-	public static Connection getConnection() throws SQLException {
-		String jdbcURL="jdbc:mysql://localhost/dizionario?user=root&password=root";
-		return DriverManager.getConnection(jdbcURL);
+public class DBConnect {
+	
+	static private final String jdbcURL="jdbc:mysql://localhost/dizionario?user=root&password=root";
+	static private HikariDataSource ds=null;//non ho un ds quando il programma parte
+	
+	public static Connection getConnection() {
+		
+		if(ds==null) {
+			
+			ds=new HikariDataSource();
+			ds.setJdbcUrl(jdbcURL);
+			//ds.setUsername("root"); -->metto uname e pwd qui se non li ho gia messi sopra
+			//ds.setPassword("root");
+		}
+		
+		try {
+			
+			Connection connection=ds.getConnection();
+			return connection;
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
